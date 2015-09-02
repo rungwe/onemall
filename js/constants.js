834eb31d3818e7b5e881eef702c673c75ad08b34
rungwe <kumbirair@yahoo.com>
2015-09-02 16:08:25 +0200

    testing local host

--- js/constants.js
+++
@@ -1,4 +0,0 @@
-//Global constants
-
-drawer="off"
-url = "http://localhost:8080/Client.php";
--- js/price.js
+++
@@ -1,8 +0,0 @@
-/*price range*/
-
- $('#sl2').slider();
-
-	var RGBChange = function() {
-	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
-	};	
-	
