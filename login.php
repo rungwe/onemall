909f7ecb450984ebbf51554e744422a68d18bef8
rungwe <kumbirair@yahoo.com>
2015-09-30 10:17:00 +0200

    fix landing pages

--- login.php
+++ login.php
@@ -33,7 +33,7 @@
 		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"> </script>
     </head>
 
-    <body>
+    <body style="width:100%;">
     
         <!-- Loader -->
     	<div class="loader" style="display: none;">
